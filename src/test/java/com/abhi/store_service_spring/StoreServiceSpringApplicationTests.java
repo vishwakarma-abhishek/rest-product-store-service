7c2c7fb6143305028900d66fdd2d9bb5578dc8b3
Abhishek Vishwakarma <avishwakarma@opentext.com>
2024-12-02 09:24:30 +0530

    Initial Commit: Project setup

---
+++ src/test/java/com/abhi/store_service_spring/StoreServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.store_service_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
